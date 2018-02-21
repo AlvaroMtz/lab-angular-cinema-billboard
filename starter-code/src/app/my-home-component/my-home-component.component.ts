14c9275438adda384bdc4bbd92b6c3f5d05fea3a
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2018-02-21 18:30:09 +0100

    Iteration3

--- starter-code/src/app/my-home-component/my-home-component.component.ts
+++ starter-code/src/app/my-home-component/my-home-component.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { MoviesService } from '../services/movies.service'
 
 @Component({
   selector: 'app-my-home-component',
   templateUrl: './my-home-component.component.html',
-  styleUrls: ['./my-home-component.component.css']
+  styleUrls: ['./my-home-component.component.css'],
+  providers: [MoviesService],
 })
 export class MyHomeComponentComponent implements OnInit {
-
-  constructor() { }
+  
+  
+  constructor( public router: Router, private listOfMovies: MoviesService) {}
 
   ngOnInit() {
+
+    //console.log(this.listOfMovies);
+    //console.log(typeof(this.listOfMovies.listOfMovies))
   }
+  hola = this.listOfMovies.getMovies()
+
 
 }
--- starter-code/src/app/services/movies.service.ts
+++ starter-code/src/app/services/movies.service.ts
@@ -1,36 +1,26 @@
 import { Injectable } from '@angular/core';
-
-export interface MovieInterface {
-    id: Number;
-    title: String;
-    poster: String;
-    synopsis: String;
-    genres: Array<string>;
-    year: Number;
-    director: String;
-    actors: Array<string>;
-    hours: Array<string>;
-    room: Number;
-}
+import movies from '../../sample-movies'
 
 @Injectable()
 export class MoviesService {
-    movies: Array<MovieInterface> = [];
-    constructor() { }
+    private listOfMovies: Array<any> = movies
+
+    constructor() {}
 
     getMovies(){
+        return this.listOfMovies ;
     }
 
     getMovie(id){
     let encontrado = false;
     let i = 0;
     while (!encontrado) {
-        if (this.movies[i].id == id)
+        if (this.listOfMovies[i].id == id)
             encontrado = !encontrado;
         else
             i++;
     }
-    return this.movies[i]
+    return this.listOfMovies[i]
             
     };
 }
